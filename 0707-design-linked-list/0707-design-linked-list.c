1f32a470f1f3c26fffa5311dc89c71b711d8c0ed
Lost-1999 <2117369751@qq.com>
2026-08-07 22:33:56 +0800

    Time: 16 ms (41.92%), Space: 20.1 MB (58.08%) - LeetHub

--- 0707-design-linked-list/0707-design-linked-list.c
+++ 0707-design-linked-list/0707-design-linked-list.c
@@ -1,89 +1,85 @@
-
-
-typedef struct MyLinkedList{
+typedef struct Node{
     int val;
-    struct MyLinkedList* next;
+    struct Node *next;
+}Node;
+
+typedef struct {
+    Node *head;
+    int size;
 } MyLinkedList;
 
+
 MyLinkedList* myLinkedListCreate() {
-    MyLinkedList *head = malloc(sizeof(MyLinkedList));
-    head->next = NULL;
-    return head;
+    MyLinkedList *list = malloc(sizeof(*list));
+    list->head = malloc(sizeof(*list->head));
+    list->head->next = NULL;
+    list->size = 0;
+    return list;
 }
 
 int myLinkedListGet(MyLinkedList* obj, int index) {
-    MyLinkedList *p;
-    int i;
-    for(i=0,p=obj->next;i<index && p!=NULL;i++,p=p->next){};
-    if(i != index)return -1;
-    else{
-        return p == NULL ? -1 : p->val;
-    }
-}
-
-void myLinkedListAddAtHead(MyLinkedList* obj, int val) {
-    MyLinkedList *p = malloc(sizeof(MyLinkedList));
-    p->next = obj->next;
-    p->val = val;
-    obj->next = p;
-}
-
-void myLinkedListAddAtTail(MyLinkedList* obj, int val) {
-    MyLinkedList *p = obj,*q = malloc(sizeof(MyLinkedList));
-    while(p->next != NULL){
+    if(index < 0 || index >= obj->size)return -1;
+    Node *p = obj->head->next;
+    while(index--){
         p = p->next;
     }
-    p->next = q;
-    q->val = val;
-    q->next = NULL;
+    return p->val;
 }
 
 void myLinkedListAddAtIndex(MyLinkedList* obj, int index, int val) {
-    MyLinkedList *p,*q = malloc(sizeof(MyLinkedList));
-    int i;
-    for(i=0,p=obj;i<index && p!=NULL;i++,p=p->next){};
-    if(i != index)return;
-    else{
-        q->next = p->next;
-        p->next = q;
-        q->val = val;
+    Node *p = obj->head;
+    Node *q = malloc(sizeof(*q));
+    if(index < 0 || index > obj->size)return;
+    for(int i=0;i<index;i++){
+        p = p->next;
     }
+    q->val = val;
+    q->next = p->next;
+    p->next = q;
+    obj->size++;
+}
+
+void myLinkedListAddAtHead(MyLinkedList* obj, int val) {
+    myLinkedListAddAtIndex(obj,0,val);
+}
+
+void myLinkedListAddAtTail(MyLinkedList* obj, int val) {
+    myLinkedListAddAtIndex(obj,obj->size,val);
 }
 
 void myLinkedListDeleteAtIndex(MyLinkedList* obj, int index) {
-    MyLinkedList *p,*q;
-    int i;
-    for(i=0,p=obj;i<index && p->next !=NULL;i++,p=p->next){};
-    if(i != index || p->next == NULL)return;
-    else{
-        q = p->next;
-        p->next = q->next;
-        free(q);
+    Node *p = obj->head;
+    if(index < 0 || index >= obj->size)return;
+    for(int i=0;i<index;i++){
+        p = p->next;
     }
+    Node *q = p->next;
+    p->next = q->next;
+    free(q);
+    obj->size--;
 }
 
 void myLinkedListFree(MyLinkedList* obj) {
-    MyLinkedList *p;
-    while(obj->next != NULL){
-        p = obj;
-        obj = obj->next;
+    Node *p = obj->head;
+    while(p != NULL){
+        Node *q = p->next;
         free(p);
+        p = q;
     }
-    free(obj);
 }
 
 /**
  * Your MyLinkedList struct will be instantiated and called as such:
  * MyLinkedList* obj = myLinkedListCreate();
  * int param_1 = myLinkedListGet(obj, index);
-
+ 
  * myLinkedListAddAtHead(obj, val);
-
+ 
  * myLinkedListAddAtTail(obj, val);
-
+ 
  * myLinkedListAddAtIndex(obj, index, val);
-
+ 
  * myLinkedListDeleteAtIndex(obj, index);
-
+ 
  * myLinkedListFree(obj);
 */
