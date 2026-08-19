91e7a8b38bdd718fc1d1c138af68c635f4e0a501
Lost-1999 <2117369751@qq.com>
2026-08-19 12:20:52 +0800

    Time: 0 ms (100%), Space: 11.4 MB (78.44%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.c
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.c
@@ -6,22 +6,14 @@
  * };
  */
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
-    struct ListNode *p,*head = NULL ,*tail = NULL;
-    while(list1 != NULL && list2 != NULL){
-        p = list1->val <= list2->val ? list1 : list2;
-        if(list1->val <= list2->val){
-            list1 = list1->next;
-        }else{
-            list2 = list2->next;
-        }
-        if(head == NULL){
-            head=p;
-        }else{
-            tail->next = p;
-        }
-        tail = p;
+    if(!list1)return list2;
+    if(!list2)return list1;
+    if(list1->val <= list2->val){
+        list1->next = mergeTwoLists(list1->next,list2);
+        return list1;
     }
-    if(tail != NULL)tail->next = list1 != NULL ? list1 : list2;
-    else head = list1 != NULL ? list1 : list2;
-    return head; 
+    else{
+        list2->next = mergeTwoLists(list1,list2->next);
+        return list2;
+    }
 }
