893232ee397a10cdaaf3612bfd8b4fb5e134d194
Lost-1999 <2117369751@qq.com>
2026-08-22 23:57:41 +0800

    Time: 14 ms (29.59%), Space: 11.8 MB (85.71%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.c
+++ 0141-linked-list-cycle/0141-linked-list-cycle.c
@@ -7,10 +7,9 @@
  */
 bool hasCycle(struct ListNode *head) {
     struct ListNode *slow,*fast;
-    if(head == NULL || head->next == NULL)return false;
     slow = head;
     fast = head;
-    while(fast->next != NULL && fast->next->next != NULL){
+    while(fast != NULL && fast->next != NULL){
         slow = slow->next;
         fast = fast->next->next;
         if(slow == fast)return true;
