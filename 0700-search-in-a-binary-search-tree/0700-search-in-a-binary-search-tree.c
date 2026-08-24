747704514fa8a7b7ac0fc8aaa207b436d10e547f
Lost-1999 <2117369751@qq.com>
2026-08-24 16:18:25 +0800

    Time: 0 ms (100%), Space: 19.3 MB (92.26%) - LeetHub

---
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.c
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+struct TreeNode* searchBST(struct TreeNode* root, int val) {
+    if(root->val == val){
+        return root;
+    }
+    else if(root->val < val && root->right != NULL){
+        return searchBST(root->right,val);
+    }
+    else if(root->val > val && root->left != NULL){
+        return searchBST(root->left,val);
+    }
+    return NULL;
+}
+
