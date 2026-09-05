1722b12c260c203130b6b42f19b2bdce91663149
Lost-1999 <2117369751@qq.com>
2026-09-05 23:09:52 +0800

    Time: 0 ms (100%), Space: 10.3 MB (4.88%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.c
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.c
@@ -6,27 +6,20 @@
  * };
  */
 struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
-    if(head == NULL || head->next == NULL)return NULL;
-    struct ListNode *slow,*mid,*fast;
-    slow = head;
-    mid = head;
-    fast = head;
-    for(int i=0;i<n-1;i++){
+    struct ListNode dummy;
+    dummy.next = head;
+    struct ListNode *slow,*fast;
+    slow = &dummy;
+    fast = &dummy;
+    for(int i=0;i<n;i++){
         fast = fast->next;
-    }
-    if(fast->next != NULL){
-        fast = fast->next;
-        mid = mid->next;
-    }
-    else{
-        return head->next;
     }
     while(fast->next != NULL){
         fast = fast->next;
-        mid = mid->next;
         slow = slow->next;
     }
-    slow->next = mid->next;
-    free(mid);
-    return head;
+    struct ListNode *target = slow->next;
+    slow->next = target->next;
+    free(target);
+    return dummy.next;
 }
