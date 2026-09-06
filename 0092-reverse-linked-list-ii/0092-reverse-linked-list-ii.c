f97fe013f7c9a54eeb6a36c38928cf9a0310e0a0
Lost-1999 <2117369751@qq.com>
2026-09-06 22:42:27 +0800

    Time: 0 ms (100%), Space: 9.4 MB (96.68%) - LeetHub

---
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.c
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseBetween(struct ListNode* head, int left, int right) {
+    struct ListNode dummy;
+    dummy.next = head;
+    struct ListNode *beforel,*l,*r,*afterr;
+    beforel = &dummy;
+    r = head;
+    for(int i=1;i<left;i++){
+        beforel = beforel->next;
+    }
+    l = beforel->next;
+    for(int i=1;i<right;i++){
+        r = r->next;
+    }
+    if(r->next != NULL)afterr = r->next;
+    else afterr = NULL;
+    struct ListNode *pred,*curr,*succ;
+    for(pred=afterr,curr=l;curr!=afterr;pred=curr,curr=succ){
+        succ = curr->next;
+        curr->next = pred;
+    }
+    beforel->next = pred;
+    return dummy.next;
+}
