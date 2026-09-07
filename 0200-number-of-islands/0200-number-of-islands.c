9fe1301c0eff2f32a8a5c7896c2958cd5c198250
Lost-1999 <2117369751@qq.com>
2026-09-07 21:06:49 +0800

    Time: 27 ms (5.16%), Space: 13.6 MB (5.45%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.c
+++ 0200-number-of-islands/0200-number-of-islands.c
@@ -13,8 +13,10 @@ int numIslands(char** grid, int gridSize, int* gridColSize) {
     int count = 0;
     for(int i=0;i<gridSize;i++){
         for(int j=0;j<gridColSize[i];j++){
-            if(grid[i][j] == '1')count++;
-            dfs(grid,i,j,gridSize,gridColSize);
+            if(grid[i][j] == '1'){
+                count++;
+                dfs(grid,i,j,gridSize,gridColSize);
+            }
         }
     }
     return count;
